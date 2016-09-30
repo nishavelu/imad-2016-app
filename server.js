a550958500002d210c1564955ce276cde919fde3
nishavelu <nishavelu1@gmail.com>
2016-09-30 21:45:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/article-one', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-tw', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-tw.html'));
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three3', function (req, res) {
