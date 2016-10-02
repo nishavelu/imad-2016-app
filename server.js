f44487fa90bafb99cc83eb36e2466bc4795536b6
nishavelu <nishavelu1@gmail.com>
2016-10-02 22:42:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,25 +55,22 @@ function createtemplate(data){
 var htmltemplate=`
 
 <html>
-<head>
-<title>
- ${title}
- </title>
+ <head>
+  <title>
+  ${title}
+  </title>
  
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link href="/ui/style.css" rel="stylesheet" />
- 
-     
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/ui/style.css" rel="stylesheet" />
  </head>
 
 <body>
-    <div class="container">
-<div>
-       <a href="/">Home</a>
-</div>
-<hr/>
-<h3>
+     <div class="container">
+       <div>
+        <a href="/">Home</a>
+       </div>
+    <hr/>
+     <h3>
     ${heading}
 </h3>
 <div>
