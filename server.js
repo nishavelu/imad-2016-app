3e9bb23219daf55094c67ecd2e57dca79b6f2378
nishavelu <nishavelu1@gmail.com>
2016-10-02 10:55:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var content ={
+var articleone ={
     title: 'Article one | Nisha',
     heading: 'Article one',
     date: 'sep 5, 2016' ,
@@ -22,8 +22,13 @@ var content ={
         This is the content formy first Oracle. This is the content formy first Oracle. This is the content formy first Oracle. This is the content formy first Oracle. This is the content formy first Oracle. This is the content formy first Oracle. 
     </p> `
 };
+function createtemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
 
-var htmltemplate=
+var htmltemplate=`
 
 <html>
 <head>
@@ -58,13 +63,17 @@ var htmltemplate=
 </div>
 </body>
 </html>
-;
+`;
+
+
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createtemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
