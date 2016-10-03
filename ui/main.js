16cb84b18a8a4c3991cb8a49e096554d7452f310
nishavelu <nishavelu1@gmail.com>
2016-10-03 21:43:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,10 @@ element.innerHTML='Hello I am Nisha. This is my new project.';
 var img=document.getelementbyId('madi');
 var marginLeft=0;
 function moveRight() {
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+1;
     img.style.marginLeft+'px';
 }
 img.onclick=function () {
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,50);
    
 };
