f579f042ac076b9bdc5afee603b8054577de780d
nishavelu <nishavelu1@gmail.com>
2016-10-04 21:47:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,6 +19,6 @@ function moveRight() {
     img.style.marginLeft + 'px';
 }
 img.onclick = function () {
-   // var interval=setInterval(moveRight,50);
+    var interval = setInterval(moveRight,50);
    
 };
