2c921e2867607596a4bcbee999cb7d735b9a6042
nishavelu <nishavelu1@gmail.com>
2016-10-04 21:33:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,6 @@ console.log('Loaded!');
 //element.innerHTML='Hello I am Nisha. This is my new project.';
 
 var img=document.getElementById('madi');
-alert(img);
 img.onclick= function()
 {
     img.style.marginLeft = '100px';
