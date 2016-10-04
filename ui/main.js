ec5b721b9b7723775342fb0b5a897fbf79cb8f4a
nishavelu <nishavelu1@gmail.com>
2016-10-04 21:14:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,16 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='Hello I am Nisha. This is my new project.';
-
-var img=document.getelementbyId('madi');
+var img=document.getElementById('madi');
+img.onclick= function()
+{img.style.marginLeft = '100px';};
+/*var img=document.getelementbyId('madi');
 var marginLeft=0;
 function moveRight() {
     marginLeft=marginLeft+1;
     img.style.marginLeft+'px';
 }
 img.onclick=function () {
-    var interval=setInterval(moveRight,50);
+   // var interval=setInterval(moveRight,50);
    
-};
+};*/
