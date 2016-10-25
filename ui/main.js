90b5345a458bb68c6523b8d8b342c2be126b103c
nishavelu <nishavelu1@gmail.com>
2016-10-25 21:54:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,16 @@
-/*IMAGE POSITION SHIFT
-console.log('Loaded!');
 
-//var element = document.getElementById('main-text');
-//element.innerHTML='this is a comment.';
+var button =document.getElementById('counter');
 
-var img=document.getElementById('madi');
-img.onclick = function()
+var counter=0;
+button.onclick = function()
 {
     img.style.marginLeft = '50px';
-    
-};*/
 
 
 
-/*ANIMATION
-var img = document.getElementById('madi');
-var marginLeft = 0 ;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight,50);
-   
-};*/
+
+var counter = counter+1 ;
+var span=document.getElementById('count')
+span.InnerHTML=counter.toString();
+
+};
