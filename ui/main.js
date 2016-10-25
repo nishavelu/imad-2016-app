9a4190c99e85b1b20a3b3d14af4955e276b208b9
nishavelu <nishavelu1@gmail.com>
2016-10-25 22:10:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,9 @@ var button =document.getElementById('counter');
 
 var counter=0;
 
-button.onclick = function()
-{
-    counter = counter+1 ;
+button.onclick = function(){
+
+    counter = counter + 1 ;
 var span=document.getElementById('count');
 
 span.InnerHTML=counter.toString();
