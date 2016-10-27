11b659d00d7062a1cf36ca2e5f952ec89c8f3b0e
nishavelu <nishavelu1@gmail.com>
2016-10-27 22:18:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,4 +25,15 @@ submit.onclick = function() {
     }
     var ul = document.getElementById('namelist');
     ul.InnerHTML= list;
+    var submit = document.getElementById('submit_btn');
+    submit.onclick = function() {
+        var names= ['name1', 'name2','name3','name4'];
+        var list ='';
+        for (var i=0;i<names.length;i++); {
+            list = '<li>' + names[i]+ '</li>';
+        }
+        var ul= document.getElementById('namelist');
+        ul.InnerHTML =list
+        
+    }
 };
