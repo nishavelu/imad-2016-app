6b214e2315ec50cb2a7daeabb06de678f25f3ac8
nishavelu <nishavelu1@gmail.com>
2016-10-31 13:48:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,10 +92,12 @@ app.get('/', function (req, res) {
 });
 
 var counter=0;
-app.get('/counter', function (req, res){
+app.get('/counter', function (req, res)
+{
     counter = counter + 1;
     res.send(counter.toString());
-});
+}
+);
 
 var names=[];
 app.get('/submit-name', function (req, res) {
