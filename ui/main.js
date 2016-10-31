498c123f5b10a036d0a5c5358f55d1b844ed0e9a
nishavelu <nishavelu1@gmail.com>
2016-10-31 13:42:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,21 +20,22 @@ var request = new XMLHttpRequest();
 request.onreadystatechange = function() {
   if (request.readyState === XMLHttpRequest.DONE) {
     
-      if(request.status === 200) {
+      if(request.status === 200)
+      {
           var counter = request.responeText;
           var span = document.getElementById('count');
           span.innerHTML = counter.toString();
       }
   }
-  //Not done yet
 };
-
 //Make the request
 request.open('GET', 'http://nishavelu.imad.hasura-app.io/counter', true);
 request.send(null);
-
-
 };
+
+
+
+
 
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
