02156b730c5385c4e3e833eb096609ac56b5078b
nishavelu <nishavelu1@gmail.com>
2016-10-31 13:39:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
+/*
+COUNTING THE NUMBER OF CLICKS
+
+button.onclick = function() {
+counter = counter + 1;
+var span=document.getElementById('count');
+span.innerHTML=counter.toString();
+};
+*/
+
+
 
 var button =document.getElementById('counter');
 
