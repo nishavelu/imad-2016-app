b826df69133dd5a1ec34113d0ac90704fa283186
nishavelu <nishavelu1@gmail.com>
2016-11-01 22:40:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,8 +39,8 @@ submit.onclick = function() {
   var request = new XMLHttpRequest();
 // capture the response and store in a variable
   request.onreadystatechange = function() {
-     if (request.readyState === XMLHttpRequest.DONE)
-      {
+     if (request.readyState === XMLHttpRequest.DONE) {
+      
         
       if (request.status===200)
        { 
@@ -48,8 +48,8 @@ submit.onclick = function() {
 	    var names=request.responseText;
 	    names=JSON.parse(names);
 	    var list='';
-        for (var i=0;i<names.length;i++)
-        {
+        for (var i=0;i<names.length;i++) {
+        
          list += '<li>' + names[i] + '</li>';
             
          }
@@ -62,7 +62,7 @@ submit.onclick = function() {
     var nameInput=document.getElementById('name');
     var name=nameInput.value;
     
-    request.open('GET', 'http://nishavelu.imad.hasura-app.io/submit-name?name=' + name ,true);
+    request.open('GET','http://nishavelu.imad.hasura-app.io/submit-name?name=' +name ,true);
     request.send(null);
  }; 
  
