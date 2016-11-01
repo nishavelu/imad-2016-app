cad3077830a1e1eec945d3741295b3bae6c0393e
nishavelu <nishavelu1@gmail.com>
2016-11-01 22:42:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,8 +50,8 @@ submit.onclick = function() {
 	    var list='';
         for (var i=0;i<names.length;i++)
         {
-          // list += '<li>' + names[i] + '</li>';
-            list +=  names[i] +'<br>';
+           list += '<li>' + names[i] + '</li>';
+            
          }
 	    var p = document.getElementById('namelist');
         p.innerHTML = list;
