d4bbb3de021576fc00085da8e575d11c9b904a81
nishavelu <nishavelu1@gmail.com>
2016-11-01 22:10:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,21 +34,36 @@ request.send(null);
 };
 
 
-
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-
+var submit =document.getElementById('submit-btn');
 submit.onclick = function() {
-
-    
-    var names= ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-        for ( var i=0; i<names.length; i++) 
+  
+  var request = new XMLHttpRequest();
+// capture the response and store in a variable
+  request.onreadystatechange = function() {
+     if (request.readyState === XMLHttpRequest.DONE)
+      {
+        
+      if (request.status===200)
+       { 
+           
+	    var names=request.responseText;
+	    names=JSON.parse(names);
+	    var list='';
+        for (var i=0;i<names.length;i++)
         {
-            list += '<li>' + names[i] + '</li>';
-        }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML= list;
-       
-  };
+          // list += '<li>' + names[i] + '</li>';
+            list +=  names[i] +'<br>';
+         }
+	    var p = document.getElementById('namelist');
+        p.innerHTML = list;
+         }
+   }        
+  
+};
+    var nameInput=document.getElementById('name');
+    var name1=nameInput.value;
+    
+    request.open('GET', 'http://nishavelu.imad.hasura-app.io/submit-name?name=' + name ,true);
+    request.send(null);
+ }; 
+ 
