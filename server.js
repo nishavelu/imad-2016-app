f2c14cb0bc2eb75c6bdade8a19d84aa84682439b
nishavelu <nishavelu1@gmail.com>
2016-11-02 22:17:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ var pg =require('pg');
 var app = express();
 app.use(morgan('combined'));
 
-var config ={
-    user:"nishavelu",
-    database:"nishavelu",
-    host: "db.imad.hasura-app.io",
-    port:"5432",
-    password: process.env.DB_PASSWORD
-};
-
 
 var articles={
 
@@ -97,22 +89,6 @@ return htmltemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var pool = new pg.Pool(config);
-
-app.get("/test-db", function (req,res){
-    // make a select request
-    // return a response with the result
-    
-    pool.query("SELECT * FROM test",function (err, result){
-       if (err){
-           res.status(500).send (err.toString());
-          } else {
-              res.send(JSON.stringify(result));
-          }
-          
-      });
 });
 
 
