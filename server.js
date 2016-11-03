dcd6579d7689d2163277bc6ebfa6bc51ec40005d
nishavelu <nishavelu1@gmail.com>
2016-11-03 14:39:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ app.get('/submit-name', function (req, res) {
 
 app.get('/articles/:articlename', function (req, res) {
 
-  pool.query("SELECT * FROM article WHERE title= '"+req.params.articleName +"'", function(err,result){
+  pool.query("SELECT * FROM article WHERE title= '"+req.params.articlename +"'", function(err,result){
     if(err){
         res.status(500).send(error.toString());
         } 
