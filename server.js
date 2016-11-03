0eaa49e4246ca5b2eaf0f19c80296b6e9716f364
nishavelu <nishavelu1@gmail.com>
2016-11-03 14:39:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,9 +136,9 @@ app.get('/submit-name', function (req, res) {
     res.send(JSON.stringify(names));
 });    
 
-app.get('/articles/:articlename', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
 
-  pool.query("SELECT * FROM article WHERE title= '"+req.params.articlename +"'", function(err,result){
+  pool.query("SELECT * FROM article WHERE title= '"+req.params.articleName +"'", function(err,result){
     if(err){
         res.status(500).send(error.toString());
         } 
