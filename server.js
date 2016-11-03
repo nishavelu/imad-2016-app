89da0c3b8f6536244b9592f395f07acea328aaf6
nishavelu <nishavelu1@gmail.com>
2016-11-03 10:28:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 
-var config ={
-    user: 'nishavelu',
-    database: 'nishavelu',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
+var config = {
+user: 'nishavelu',
+database: 'nishavelu',
+host: 'db.imad.hasura-app.io',
+port: '5432',
+password: process.env.DB_PASSWORD
 };
 
 
