1e68e1e85e691cef66bddd1701b757d5fd692aaf
nishavelu <nishavelu1@gmail.com>
2016-11-30 22:21:46 +0530

    Create article.js

---
+++ ui/article.js
@@ -0,0 +1,7 @@
+function escapeHTML (text)
+  {
+ -    var $text = document.createTextNode(html);
+ +    var $text = document.createTextNode(text);
+      var $div = document.createElement('div');
+      $div.appendChild($text);
+      return $div.innerHTML;
