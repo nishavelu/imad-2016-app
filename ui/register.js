3e040d1a34bd9a7b3a5f64012a33779793f3efd1
nishavelu <nishavelu1@gmail.com>
2016-12-01 21:57:47 +0530

    Create register.js

---
+++ ui/register.js
@@ -0,0 +1,26 @@
+register.onclick = function () {
+  var newRequest = new XMLHttpRequest();
+  //capture the response and store it in a variable
+  newRequest.onreadystatechange =function () {
+    if(newRequest.readyState === XMLHttpRequest.DONE){
+      if(newRequest.status === 200){
+          alert('Registration successful');
+      }
+      else if(newRequest.status ===500){
+          alert('Something went wrong with the server');
+        }
+    }
+  };
+  
+  //registration module
+  var newUsername = document.getElementById('user').value;
+  var newPassword = document.getElementById('pass').value;
+  console.log(newUsername);
+  console.log(newPassword);
+   //make the request
+    newRequest.open('POST','http://rohitsinghcse.imad.hasura-app.io/register-user');
+    newRequest.setRequestHeader('Content-Type','application/json');
+    newRequest.send(JSON.stringify({uname:newUsername,passwd:newPassword}));
+    
+};
+
