f3eca334c1072dd9ee72528380ff393a6b70bdd2
nishavelu <nishavelu1@gmail.com>
2016-12-01 21:33:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,24 @@ app.get('/', function (req, res) {
 
 
 
+
 var pool = new pg.Pool(config);
+
+app.get('/test-db', function (req,res){
+    // make a select request
+    // return a response with the result
+    
+    pool.query('SELECT * FROM test',function(err, result){
+       if (err){
+           res.status(500).send(err.toString());
+          } else {
+              res.send(JSON.stringify(result.rows));
+          }
+          
+      });
+});
+
+
 
 var counter=0;
 app.get('/counter', function (req, res)
