283ff61833f3bd32883f7a0ef5181fe0f588113d
nishavelu <nishavelu1@gmail.com>
2016-12-02 13:21:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -283,7 +283,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/register.js',function(req,res){
+app.get('/register.js',function(req,res){
     res.sendFile(path.join(__dirname,'ui','register.js'));
     
 });
