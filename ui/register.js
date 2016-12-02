043ba86a237e0a441c4cf7cea51978259339bac2
nishavelu <nishavelu1@gmail.com>
2016-12-02 21:13:42 +0530

    [imad-console] Updates ui/register.js

--- ui/register.js
+++ ui/register.js
@@ -1,5 +1,6 @@
 var register = document.getElementById('register_btn');
 register.onclick = function () {
+     alert('hello');
   var newRequest = new XMLHttpRequest();
   //capture the response and store it in a variable
   newRequest.onreadystatechange = function () {
