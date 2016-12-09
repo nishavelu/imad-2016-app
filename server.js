53f4e258b2de18afe15abfb1a15770188e359d0c
nishavelu <nishavelu1@gmail.com>
2016-12-09 19:53:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -300,7 +300,7 @@ app.get('/get-articles', function (req, res) {
 app.get('/get-comments/:articleName', function (req, res) {
    // make a select request
    // return a response with the results
-   pool.query('SELECT comment.*, "user_testing".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
+   pool.query('SELECT comment.*, "user_testing".username FROM article, comment, "user_testing" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user_testing".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
